a853d0556c59dd35c18ced48b048b860dae8c281
Nate Miller <natekmiller96@gmail.com>
2018-08-11 23:34:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,17 @@
+require_relative 'config/environment'
+
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index
-	end
-
+  get '/date' do
+    erb :date
+  end
+  
+  get '/hello' do
+    erb :hello
+  end
+  
+  get '/goodbye' do
+    erb :goodbye
+  end
 
 end
