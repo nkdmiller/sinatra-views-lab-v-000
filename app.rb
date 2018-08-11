9e802f5d358a5e9b1ba6f7a3d8dfe4bdff012b1d
Nate Miller <natekmiller96@gmail.com>
2018-08-11 23:03:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,8 +1,17 @@
+require_relative 'config/environment'
+
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index
-	end
-
+  get '/' do
+    erb :index
+  end
+  
+  get '/hello' do
+    erb :hello
+  end
+  
+  get '/goodbye' do
+    erb :goodbye
+  end
 
 end
